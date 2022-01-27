7a3f9a05a5c0542786cd19ed47dbf9549ac65bad
BarrySploit <34023280+BarrySploit@users.noreply.github.com>
2022-01-27 11:27:09 -0500

    Update PureLuckSolve.py

--- CyberTalents/PureLuckSolve.py
+++ CyberTalents/PureLuckSolve.py
@@ -1,9 +1,18 @@
+#open file as flaglist variable
 with open('./flaglist.txt', 'r') as flaglist:
+	
+	#add all values to list
 	flag = []
+	
+	#strip unnecessary extras like \n. I manually deleted the 0x earlier.
 	for i in flaglist:
 		new = i.strip('\n')
+		
+		#convert hex bytes to ascii string
 		decode = bytes.fromhex(new).decode('utf-8')
 		flag.append(decode)
+	
+	#add each value in list to a string for easy copy and paste flag.
 	finalstring = ''
 	for i in flag:
 		finalstring += i
