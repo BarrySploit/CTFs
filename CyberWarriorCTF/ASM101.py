11ca3c01ae1a2f912332bb63aa9985999464503e
BarrySploit <34023280+BarrySploit@users.noreply.github.com>
2022-02-07 11:39:22 -0700

    Update ASM101.py

--- CyberWarriorCTF/ASM101.py
+++ CyberWarriorCTF/ASM101.py
@@ -10,16 +10,16 @@ sub_number = 133337
 for i in range(0000,99999999):
 	
   #Convert to string and pad with leading zeros since input has to be at least 4 bytes (according to the assembly code)
-  beginning = str(i).zfill(4)
+	beginning = str(i).zfill(4)
   
   #convert back to int and start following the assembly
 	first_step = int(beginning) ^ xor_number
 	second_step = first_step + add_number
 	third = second_step - sub_number
-	#This is where it gets tricky. EAX * EAX * EAD basically means EAX * 3, don't get confused.
-  fourth = third * 3
-	#Another tricky EAX * 3.
-  fifth = fourth * 3
+  #This is where it gets tricky. EAX * EAX * EAD basically means EAX * 3, don't get confused.
+  	fourth = third * 3
+  #Another tricky EAX * 3.
+  	fifth = fourth * 3
   #EAX * EAX
 	sixth = fifth * 2
   #Found out SAL means * 2 in this instance
