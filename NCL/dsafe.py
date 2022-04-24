f7abca704de9ee173de82db7a7ba5f3039b9ae46
BarrySploit <34023280+BarrySploit@users.noreply.github.com>
2022-04-24 16:35:09 -0600

    Update dsafe.py

--- NCL/dsafe.py
+++ NCL/dsafe.py
@@ -1,7 +1,7 @@
 #challenge provides code that takes number higher than 47
 #compares first four characters of sha256 hash against a12c
 #this just bruteforces and prints out the numbers that it could be
-#I'm sure I could consolidate some fo the lines in the loop, but it works so fuck it lol
+#I'm sure I could consolidate some fo the lines in the loop, but it works! lol
 import hashlib
 key = "a12c"
 for i in range(47, 1000000):
