08471546812a18849af66f6d81ffd078a4694bc7
BarrySploit <34023280+BarrySploit@users.noreply.github.com>
2022-05-25 17:39:45 -0600

    Add files via upload

---
+++ Powershell Practice/HashTester.ps1
@@ -0,0 +1,8 @@
+﻿$knownhash = "6859e1d10d08c1ea91f6e53ba6d601149b08d4efab8f8c2d586f6858ae1773a7"
+$directory = ls .\bin
+foreach ($i in $directory){
+    $hashes = Get-FileHash .\bin\$i
+    if ($hashes.hash -eq $knownhash){
+        $hashes
+    }
+}
