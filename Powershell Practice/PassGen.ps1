3b636da6a94b3ebc610944cf46440d70085aeb48
BarrySploit <34023280+BarrySploit@users.noreply.github.com>
2022-05-26 22:25:24 -0600

    Add files via upload

---
+++ Powershell Practice/PassGen.ps1
@@ -0,0 +1,22 @@
+﻿$count = Read-Host -Prompt "How long would you like your password?"
+$upper = [char[]] (([char]'A')..([char]'Z'))
+$lower = [char[]] (([char]'a')..([char]'z'))
+$numbers = [char[]] (([char]'0')..([char]'9'))
+$special = '!','@','#','$','%','^','&','*','(',')'
+$total = $upper+$lower+$numbers+$special
+$password = Get-Random -InputObject $total -Count $count
+Write-Host "This is your password!! with a length of $count!"
+$test = ''
+foreach($i in $password){
+    $test += $i
+    }
+$test
+$password
+$email = Read-Host -Prompt "What email is this associated with?"
+$domain = Read-Host -Prompt "What website is this used for?"
+$passobj =  New-Object psobject -Property @{
+    'Email' = $email
+    'Website' = $domain
+    'Password' = $test
+    }
+$passobj | Export-Csv -Path .\test2.csv
