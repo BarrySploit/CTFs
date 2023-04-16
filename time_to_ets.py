344dcad64bce5c4a488e8f0fc2f3ebfbd6510c2b
BarrySploit <34023280+BarrySploit@users.noreply.github.com>
2023-04-15 21:50:15 -0600

    Update time_to_ets.py

--- time_to_ets.py
+++ time_to_ets.py
@@ -1,5 +1,7 @@
 import time
 from win10toast import ToastNotifier
+import requests
+
 
 ets_leave = 1688968800.0000000
 current_time = time.time()
@@ -10,6 +12,8 @@ hours = int(minutes)/60
 leftover_hours = int(hours) % 24
 days = int(hours)/24
 final = f'{int(days)} days, {int(leftover_hours)} hours, and {int(leftover_min)} minutes to FREEDOM!'
+#windows notification
+print("Sending windows notification")
 toast = ToastNotifier()
 while True:
 	toast.show_toast(
@@ -19,4 +23,12 @@ while True:
 		icon_path = ".\\download.ico",
 		threaded = True,
 	)
-	time.sleep(600)
+
+#android notification
+print("sending telegram message")
+id = "6218744091"
+token = "6012387725:AAFOgyxlgmyxZ-4vJVwPIzF6Q7UfhrB7SKw"
+url = f"https://api.telegram.org/barry_ets_bot{token}/sendMessage"
+params = {"chat_id": id, "text": final}
+r = requests.get(url=url,params=params)
+time.sleep(60)
